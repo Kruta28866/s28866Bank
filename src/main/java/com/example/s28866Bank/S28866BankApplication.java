82afb262cd1881df550b221efbd446b4013ca65d
Kruta28866 <s28866@pjwstk.edu.pl>
2024-01-27 17:29:15 +0100

    Initial commit

---
+++ src/main/java/com/example/s28866Bank/S28866BankApplication.java
@@ -0,0 +1,13 @@
+package com.example.s28866Bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S28866BankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S28866BankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/s28866Bank/S28866BankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.s28866Bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S28866BankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
